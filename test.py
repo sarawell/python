f3ed36f847d34c123b81446c6462927cac966299
sarawell <664943056@qq.com>
2018-11-24 17:21:16 +0800

    get url

--- test.py
+++ test.py
@@ -1,2 +1,25 @@
+#!/usr/bin/python3
+# -*- coding: UTF-8 -*-
 
-print("hello world")
+
+from urllib import request
+from  bs4 import BeautifulSoup
+import re
+import time
+
+url="http://www.jianshu.com"
+headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36'}
+page = request.Request(url,headers=headers)
+page_info = request.urlopen(page).read().decode('utf-8')
+
+
+# 将获取到的内容转换成BeautifulSoup格式，并将html.parser作为解析器
+soup = BeautifulSoup(page_info, 'html.parser')
+# 以格式化的形式打印html
+#print(soup.prettify())
+ 
+titles = soup.find_all('a', 'title')# 查找所有a标签中class='title'的语句
+
+for title in titles:
+    print(title.string+"\n")
+    print("http://www.jianshu.com" + title.get('href')+"\n\n")
