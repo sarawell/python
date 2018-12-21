e766bfe5ea93983ae5881096f242874b9ae85cbf
sarawell <664943056@qq.com>
2018-12-20 22:50:51 -0800

    1

--- analysisJianShu.py
+++ analysisJianShu.py
@@ -5,12 +5,17 @@ from  bs4 import BeautifulSoup
 import re
 import time
 import sqlite3
+from lxml import etree
+import json
+import datetime
+#from lxml.etree import HTMLParser
 
 
 class AnalysisAuthor:
     info=[]
     following_name=[]
     follower_name=[]
+    title_href=[]
     url_header="https://www.jianshu.com/"
     def __init__(self,url):
         self.url=self.url_header+"/u/"+url
@@ -101,39 +106,118 @@ class AnalysisAuthor:
             
         return self.follower_name
 
-    #def GetTitle(self):
+    def GetTitle(self):
+        self.tilist=[]
+        self.t_page_num=2
+        self.title=self.soup.find_all('a','title')
+        for self.ti in self.title:
+            print(self.ti.get('href')[3:len(self.ti.get('href'))])
+            self.tilist.append(self.ti.get('href')[3:len(self.ti.get('href'))])
+            self.title_href.append(self.ti.get('href')[3:len(self.ti.get('href'))])
+        while(len(self.tilist)>8):
+            self.tilist.clear()
+            self.t_url=self.url_header+"/u/"+url+"?order_by=shared_at&page="+str(self.t_page_num)
+            print(self.t_url)
+            self.t_page=request.Request(self.t_url,headers=self.headers)
+            self.page_info_t=request.urlopen(self.t_page).read().decode('utf-8')
+            self.soup_t=BeautifulSoup(self.page_info_t,'html.parser')
+            self.title=self.soup_t.find_all('a','title')
+            for self.ti in self.title:
+                print(self.ti.get('href')[3:len(self.ti.get('href'))])
+                self.tilist.append(self.ti.get('href')[3:len(self.ti.get('href'))])
+                self.title_href.append(self.ti.get('href')[3:len(self.ti.get('href'))])
+            
+            self.t_page_num=self.t_page_num+1
+        return self.title_href
 
 
-class AnalysisTitle:
+
+
+            #https://www.jianshu.com/u/9ed50acac61c?order_by=shared_at&page=5
+
+
+
+
+
+
+
+
+
+class GetTitleinfo:
     url_header="https://www.jianshu.com/"
+    
     def __init__(self,url):
-        #self.url=url
-        self.url=self.url_header+"/u/"+url
+        self.url=self.url=self.url_header+"p/"+url
+        print(self.url)
         self.headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36'}
         self.page = request.Request(self.url,headers=self.headers)
         self.page_info = request.urlopen(self.page).read().decode('utf-8')
-        self.soup = BeautifulSoup(self.page_info, 'html.parser')
+        self.selector=etree.HTML(self.page_info)
+        self.test=self.selector.xpath("//script[@type='application/json']")
+        self.author_json=json.loads(self.test[0].text)
+        print(self.author_json)
 
-    def GetTitleHref(self):
-        self.titlehref=self.soup.find_all('a','title')
-        for self.i in self.titlehref:
-            print(self.i.get('href')[3:len(self.i.get('href'))])
-        while()
+    def Getwordage(self):        
+        return self.author_json['note']['public_wordage']
+        
+    def GetPubilc_time(self):
+        self.public_time=self.selector.xpath("//span[@class='publish-time']")
+        #print(self.public_time[0].get('title'))
+        self.time=self.public_time[0].get('title')[6:len(self.public_time[0].get('title'))]
+        self.dt=datetime.datetime.strptime(self.time, "%Y.%m.%d %H:%M")
+        return self.dt
 
+    def GetViewcount(self):       
+        return self.author_json['note']['views_count']
     
+    def GetLikecount(self):
+        return self.author_json['note']['likes_count']
+        
+    def GetCommondcount(self):
+        return self.author_json['note']['comments_count']
+    
+    def GEtAuthorName(self):
+        self.getname=self.selector.xpath("//a[@class='avatar']")
+        self.temp=self.getname[0].get('href')
+        return self.temp[3:len(self.temp)]
+        
+       
+
+        
+      
 
 
 
 
 
 
+#mian
 
 
 
-url="9ed50acac61c"
 
-aaa=AnalysisTitle(url)
-aaa.GetTitleHref()
+""" url="74b0e0bfe47a"
+aaa=GetTitleinfo(url)
+#aaa.qtest()
+print(aaa.GetPubilc_time())
+print(type(aaa.GetPubilc_time()))
+print(aaa.GetViewcount())
+print(aaa.GetCommondcount())
+print(aaa.GetLikecount())
+print(aaa.Getwordage())
+print(type(aaa.GetViewcount()))
+print(aaa.GEtAuthorName()) """
+""" aaa=AnalysisTitle(url)
+aaa.GetTitleName()
+aaa.GetAuthor()
+aaa.GetPubilc_time()
+aaa.GetWordage() """
+#aaa=AnalysisAuthor(url)
+#aaa.GetTitle()
+#print(len(aaa.GetTitle()))
+
+#aaa=AnalysisTitle(url)
+#aaa.GetTitleHref()
 
 """ 
 aaa=AnalysisAuthor(url)
